25bde5c6c45f2b53dc68e4f4962dd3e7970a0a9e
Ayberk Kolcu <ayberkkolcu@gmail.com>
2022-09-26 12:58:50 +0100

    Unusing lines removerd.

--- MVCSQSAZURELastExcercise.NetCore/Services/Sender.cs
+++ MVCSQSAZURELastExcercise.NetCore/Services/Sender.cs
@@ -1,14 +1,9 @@
 ﻿using Amazon;
-using Amazon.Runtime;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using Azure.Messaging.ServiceBus;
-using Microsoft.Azure.ServiceBus;
 using ProduceMessageServices.NetCore.Models;
 using SenderQueueMessageServices.NetCore.Configuration;
-using System.Text;
-using System.Text.Json;
-using Message = Microsoft.Azure.ServiceBus.Message;
 
 namespace SenderQueueMessageServices.NetCore.Services
 {
